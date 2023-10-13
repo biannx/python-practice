01a5c2d58fcb8b99d286e0cfdc4f44fbb0c70256
Bianca Aquino <lyahbianca.aquino04@gmail.com>
2023-10-13 22:52:39 +0800

    imports and modules

---
+++ imports.py
@@ -0,0 +1,19 @@
+from life import theAnswer
+from life import petName
+import life
+import math
+
+# using the from life import variable)
+print(theAnswer)
+print(petName)
+
+# using import
+name = life.name
+print(name)
+
+answer_squareroot = life.square_root(225)
+print(f'Squareroot: {answer_squareroot}' )
+
+# using math
+print(math.pi)
+print(math.e)
---
+++ life.py
@@ -0,0 +1,9 @@
+import math
+
+theAnswer = 35
+name = "Bianca"
+petName = "Boie and Kelly"
+
+def square_root(x):
+    return math.sqrt(x)
+
